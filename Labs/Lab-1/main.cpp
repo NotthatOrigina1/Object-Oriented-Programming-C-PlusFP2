33980431c3b5d5b001f6b84873c5c80e1c07a592
NotthatOrigina1 <157755892+NotthatOrigina1@users.noreply.github.com>
2024-02-05 20:26:33 -0800

    Add files via upload

---
+++ Labs/Lab-1/main.cpp
@@ -0,0 +1,13 @@
+/***
+ * {This program will display the message "Hello World"}
+***/  
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n" << endl;
+  printf("Hello World Again!\n");
+  return 0;
+}
